0d12fd56cd21c69dc6bf9a54f7e446b0e63581b4
Tasnu31 <tasnubaraha12@gmail.com>
2024-04-27 01:12:21 +0600

    .

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ document.addEventListener('DOMContentLoaded', () => {
         showAllButton.classList.add('d-none'); // Hide the "SHOW ALL" button
         return;
       }
+
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`);
+      const data = await response.json();
+  
+      if (data.meals) {
+        mealContainer.innerHTML = ''; // Clear previous results
+        mealResults.innerHTML = ''; // Clear previous results
+  
+        data.meals.slice(0, 5).forEach(meal => {
+          const mealCard = createMealCard(meal);
+          mealContainer.appendChild(mealCard);
+        });
