48aad4766327d4facf3b0afaccd20a886dcdcaf4
Tasnu31 <tasnubaraha12@gmail.com>
2024-04-27 00:53:27 +0600

    .

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const showAllButton = document.getElementById('showAllButton');
   
     searchInput.addEventListener('input', debounce(searchMeal, 500));
+
+    searchInput.addEventListener('input', debounce(searchMeal, 500));
+  
+    async function searchMeal() {
+      const searchTerm = searchInput.value.trim();
+  
+      if (searchTerm === '') {
+        mealContainer.innerHTML = ''; // Clear previous results
+        mealResults.innerHTML = '';
+        showAllButton.classList.add('d-none'); // Hide the "SHOW ALL" button
+        return;
+      }
